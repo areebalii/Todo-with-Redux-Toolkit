b9117611c53e18687a8f90460d287edba21bd3ec
areebalii <areebktk8@gmail.com>
2025-05-18 19:56:16 -0700

    first commit

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "this is dummy text" }]
+}
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload
+      }
+      state.todos.push(todo)
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => {
+        return todo.id !== action.payload
+      })
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            text: action.payload.text
+          }
+        }
+        return todo
+      })
+    }
+  }
+})
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions
+
+export default todoSlice.reducer
